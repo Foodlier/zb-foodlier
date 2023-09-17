5015fa783d2a79c9be5d609d9fd6117faf12670d
big-choi <bos33210@naver.com>
2023-09-18 00:47:30 +0900

    init commit

---
+++ src/test/java/com/zerobase/Foodlier/FoodlierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.Foodlier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodlierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
