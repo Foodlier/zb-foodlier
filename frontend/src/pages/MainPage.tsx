2cf4ac191075c18df5a6a81275e18b96f2bdeba6
minji-yun <minpaper.dev@gmail.com>
2023-09-20 23:51:16 +0900

    Design:기본 디자인 삭제 및 임시 Main 생성
    프로젝트 기본 템플릿 삭제 후 Header, BottomNavigation import할 Main Page 생성하였습니다.

---
+++ frontend/src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import BottomNavigation from '../components/BottomNavigation'
+import Header from '../components/Header'
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <BottomNavigation />
+    </>
+  )
+}
+
+export default MainPage
