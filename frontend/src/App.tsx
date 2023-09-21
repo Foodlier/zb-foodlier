46e485cd313b3930248d9fbad7ceda6a22ee9e45
big-choi <bos33210@naver.com>
2023-09-21 14:33:00 +0900

    Design: 로그인 화면 UI 구현
    react-router 도입 전에 홈 화면을 로그인 화면으로 구성 후에 구조 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,39 +1,109 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import styled from 'styled-components'
+import logo from './assets/logo.svg'
+import google from './assets/google.svg'
+import kakao from './assets/kakao.svg'
+import './index.css'
+
+// 스타일드 컴포넌트
+// TODO
+const Container = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
+
+const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 50px;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  max-width: 400px;
+`
+
+const Input = styled.input`
+  height: 30px;
+  padding: 10px;
+  margin-bottom: 15px;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  font-size: 16px;
+`
+
+const Button = styled.button`
+  height: 55px;
+  padding: 10px;
+  margin-bottom: 10px;
+  background-color: #e45141;
+  color: #fff;
+  border: none;
+  border-radius: 12px;
+  font-size: 16px;
+  cursor: pointer;
+`
+
+// 구분선 컴포넌트
+const Divider = styled.div`
+  width: 90%;
+  height: 1px;
+  background-color: #ccc;
+  margin: 10px 0;
+`
+
+const RegisterButton = styled(Button)`
+  background-color: #fff;
+  margin-top: 10px;
+  color: #e45141;
+  border: 1px solid #e45141;
+`
+
+const SocialLoginButton = styled(Button)`
+  width: 80%;
+  background-color: #fff;
+  margin-top: 10px;
+  color: #000;
+  border: 1px solid #393939;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [message, setMessage] = useState('')
+  // const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    fetch('/api/test')
-      .then(response => response.json())
-      .then(json => setMessage(json.SUCCESS_TEXT))
-  }, [])
+  // useEffect(() => {
+  //   fetch('/api/test')
+  //     .then(response => response.json())
+  //     .then(json => setMessage(json.SUCCESS_TEXT))
+  // }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>{message}</p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container>
+      <Title>
+        <img src={logo} alt="React Logo" />
+      </Title>
+      <Form>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Password" />
+        <Button type="submit">로그인</Button>
+        <RegisterButton>회원가입</RegisterButton>
+      </Form>
+      <Divider />
+      <SocialLoginButton>
+        <img src={google} alt="" />
+        <span>Sign in With Google</span>
+      </SocialLoginButton>
+      <SocialLoginButton>
+        <img src={kakao} alt="" />
+        <span>Sign in With Kakao</span>
+      </SocialLoginButton>
+    </Container>
   )
 }
 
