2d3081461b15f1e106970bc9341b67b588f77a5f
big-choi <bos33210@naver.com>
2023-09-22 14:14:55 +0900

    Style: 소셜로그인 아이콘 수정
    네이버, 카카오로 소셜로그인 확정에 따라 아이콘 및 CSS 수정

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,29 +1,35 @@
-import logo from '../assets/logo.svg'
-import google from '../assets/google.svg'
-import kakao from '../assets/kakao.svg'
-import * as S from '../styles/Login.styled'
+import logo from '../assets/foodlier_logo.png'
+import naver from '../assets/btn_naver.png'
+import kakao from '../assets/btn_kakao.png'
+import * as L from '../styles/LoginPage.styled'
 
-export default function Login() {
+const Login = () => {
   return (
-    <S.Container>
-      <S.Title>
-        <img src={logo} alt="React Logo" />
-      </S.Title>
-      <S.Form>
-        <S.Input type="email" placeholder="Email" />
-        <S.Input type="password" placeholder="Password" />
-        <S.Button type="submit">로그인</S.Button>
-        <S.RegisterButton>회원가입</S.RegisterButton>
-      </S.Form>
-      <S.Divider />
-      <S.SocialLoginButton>
-        <img src={google} alt="" />
-        <span>Sign in With Google</span>
-      </S.SocialLoginButton>
-      <S.SocialLoginButton>
-        <img src={kakao} alt="" />
-        <span>Sign in With Kakao</span>
-      </S.SocialLoginButton>
-    </S.Container>
+    <L.Container>
+      <L.Title>
+        <img src={logo} alt="Foodlier Logo" />
+      </L.Title>
+      <L.Form>
+        <L.Input type="email" placeholder="Email" />
+        <L.Input type="password" placeholder="Password" />
+        <L.Button type="submit">로그인</L.Button>
+        <L.RegisterButton>회원가입</L.RegisterButton>
+      </L.Form>
+      <L.Divider>
+        <L.Text>OR</L.Text>
+      </L.Divider>
+      <L.Form>
+        <L.SocialLoginButton>
+          <L.SocialLogo src={naver} alt="" />
+          <span>Sign in With Naver</span>
+        </L.SocialLoginButton>
+        <L.SocialLoginButton>
+          <L.SocialLogo src={kakao} alt="" />
+          <span>Sign in With Kakao</span>
+        </L.SocialLoginButton>
+      </L.Form>
+    </L.Container>
   )
 }
+
+export default Login
