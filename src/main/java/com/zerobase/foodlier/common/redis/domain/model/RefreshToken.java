89e94dffc8257e4ef27fa387fcbd7069b779b9a3
gomljo <gomljo0307@naver.com>
2023-09-23 22:21:54 +0900

    feat: redis에 저장할 refreshToken 객체 정의
    JPA의 CrudRepository를 활용하여redis에 저장할 계획이므로
    CrudRepository에서 사용되는 refreshToken 객체 정의
    
    resolves: #15

---
+++ src/main/java/com/zerobase/foodlier/common/redis/domain/model/RefreshToken.java
@@ -0,0 +1,28 @@
+package com.zerobase.foodlier.common.redis.domain.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@RedisHash(value = "RefreshToken") // refresh token의 만료 기간: 24시간
+public class RefreshToken {
+
+    @Id
+    private String email;
+
+    private String refreshToken;
+
+    @TimeToLive
+    private Long expiration;
+
+}
