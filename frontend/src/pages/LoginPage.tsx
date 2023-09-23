8930e55f716a00a02d2ec3b171be6bf46aa9c952
big-choi <bos33210@naver.com>
2023-09-23 14:53:04 +0900

    Feat: 로그인 페이지 기능 수정
    페이지 이동 구현을 위해
    - onClick event 추가
    - useNavigate 추가
    - styeld-component 공통 스타일 추가

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,19 +1,29 @@
+import { useNavigate } from 'react-router-dom'
 import logo from '../assets/foodlier_logo.png'
 import naver from '../assets/btn_naver.png'
 import kakao from '../assets/btn_kakao.png'
 import * as L from '../styles/LoginPage.styled'
 
 const Login = () => {
+  const navigate = useNavigate()
+
   return (
     <L.Container>
       <L.Title>
-        <img src={logo} alt="Foodlier Logo" />
+        <L.Logo src={logo} alt="Foodlier Logo" />
       </L.Title>
       <L.Form>
         <L.Input type="email" placeholder="Email" />
         <L.Input type="password" placeholder="Password" />
+        <L.FindPassword>비밀번호 찾기</L.FindPassword>
         <L.Button type="submit">로그인</L.Button>
-        <L.RegisterButton>회원가입</L.RegisterButton>
+        <L.RegisterButton
+          onClick={() => {
+            navigate('/register')
+          }}
+        >
+          회원가입
+        </L.RegisterButton>
       </L.Form>
       <L.Divider>
         <L.Text>OR</L.Text>
