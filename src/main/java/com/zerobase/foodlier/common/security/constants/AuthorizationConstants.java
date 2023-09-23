2189b968e1327e7aa383db46c56229bb1278b565
gomljo <gomljo0307@naver.com>
2023-09-23 22:52:45 +0900

    feat: json web token provider 구현
    구현 목록
    - access token 발급
    - refresh token 발급
    - token 검증
    - token 재발급
    - refresh token 삭제
    
    resolves: #15

---
+++ src/main/java/com/zerobase/foodlier/common/security/constants/AuthorizationConstants.java
@@ -0,0 +1,12 @@
+package com.zerobase.foodlier.common.security.constants;
+
+public final class AuthorizationConstants {
+
+    public static final String TOKEN_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String REFRESH_HEADER = "refresh";
+
+    private AuthorizationConstants(){
+
+    }
+}
