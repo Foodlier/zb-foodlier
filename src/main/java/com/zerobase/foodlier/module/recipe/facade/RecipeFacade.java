6ca804f697ff907877647ea58f7d9bd1f70e4ac4
Taewon Hwang <htw1203@gmail.com>
2023-09-23 22:26:25 +0900

    "Feat: 꿀조합 레시피 dto 추가"

---
+++ src/main/java/com/zerobase/foodlier/module/recipe/facade/RecipeFacade.java
@@ -0,0 +1,9 @@
+package com.zerobase.foodlier.module.recipe.facade;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecipeFacade {
+
+}
