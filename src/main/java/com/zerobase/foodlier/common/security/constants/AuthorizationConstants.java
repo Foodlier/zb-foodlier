43dd3f73d21b409ced967c44c07ebd5378f1cc12
gomljo <gomljo0307@naver.com>
2023-09-23 23:33:31 +0900

    refactor: refresh token header 값 변경
    api 명세에 정의된 refresh token header 값과 상이하여 수정
    
    resolves: #15

--- src/main/java/com/zerobase/foodlier/common/security/constants/AuthorizationConstants.java
+++ src/main/java/com/zerobase/foodlier/common/security/constants/AuthorizationConstants.java
@@ -4,7 +4,7 @@ public final class AuthorizationConstants {
 
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer";
-    public static final String REFRESH_HEADER = "refresh";
+    public static final String REFRESH_HEADER = "RefreshToken";
 
     private AuthorizationConstants(){
 
