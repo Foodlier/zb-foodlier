34bb1dd964972fba7a2786e623da2ac913738e9a
seonohmin <seon.o.min@gmail.com>
2023-09-23 18:03:24 +0900

    - styled-components를 통한 Button, Dialog UI 컴포넌트 구현과 메인  슬라이드 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,18 @@
-import './reset.css'
+// import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import LoginPage from './pages/LoginPage'
+import MainPage from './pages/MainPage'
+import Header from './components/Header'
+import BottomNavigation from './components/BottomNavigation'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // useEffect(() => {
-  //   fetch('/api/test')
-  //     .then(response => response.json())
-  //     .then(json => setMessage(json.SUCCESS_TEXT))
-  // }, [])
-
   return (
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+      <BottomNavigation />
+    </>
   )
 }
 
