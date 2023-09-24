213bb5eed79c097a921ddde5dbec4089f8a5ee80
Taewon Hwang <htw1203@gmail.com>
2023-09-24 20:01:19 +0900

    "Feat: s3이미지 수정, 삭제, 에러핸들링"

--- src/main/java/com/zerobase/foodlier/global/request/controller/S3Controller.java
+++ src/main/java/com/zerobase/foodlier/global/request/controller/S3Controller.java
@@ -1,22 +1,19 @@
 package com.zerobase.foodlier.global.request.controller;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.zerobase.foodlier.global.request.service.S3Service;
+import com.zerobase.foodlier.common.s3.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class S3Controller {
 
-    private final AmazonS3 amazonS3;
     private final S3Service s3Service;
 
     @Value("${cloud.aws.bucket}")
@@ -24,11 +21,18 @@ public class S3Controller {
 
     @PostMapping("/aws")
     public ResponseEntity<?> saveFile(@RequestPart MultipartFile multipartFile) throws IOException {
-        return ResponseEntity.
-                ok(
-                        s3Service.getImageUrl(multipartFile)
-                );
+        return ResponseEntity.ok(s3Service.getImageUrl(multipartFile));
+    }
 
+    @PostMapping("/awsList")
+    public ResponseEntity<?> saveFile(@RequestPart List<MultipartFile> multipartFileList) throws IOException {
+        return ResponseEntity.ok(s3Service.getImageUrlList(multipartFileList));
+    }
+
+    @DeleteMapping("/aws")
+    public ResponseEntity<?> deleteFile(@RequestParam String fileName) throws IOException {
+        s3Service.deleteImage(fileName);
+        return ResponseEntity.ok("delete complete");
     }
 
 }
