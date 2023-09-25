57e7cd0787fe86f2152ad1346d8ec7187b7ccf9f
Boseong Choi <bos33210@naver.com>
2023-09-25 15:51:36 +0900

    Revert "Merge branch 'master' into develop"
    This reverts commit 9b3c8a698d30d8141c60a0a9df940bad05fae6b5, reversing
    changes made to 76d8604108f37551748dba594fbf62ad2813b530.

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -48,3 +48,5 @@ const Login = () => {
     </L.Container>
   )
 }
+
+export default Login
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,29 +1,5 @@
-import styled from 'styled-components'
-import SlickSlider from '../components/slider/Slider'
-import { slides } from '../components/slider/Slide'
-
-const SlickContainer = styled.section`
-  margin-top: 5rem;
-`
-
-const Tit = styled.h1`
-  padding-left: 2rem;
-  font-size: 2.5rem;
-  font-family: 'Pretendard-ExtraBold';
-
-  @media screen and (min-width: 768px) {
-    padding-left: 2%;
-    font-size: 3rem;
-  }
-`
-
 const MainPage = () => {
-  return (
-    <SlickContainer>
-      <Tit>오늘 이 요리 어때요?</Tit>
-      <SlickSlider slides={slides} />
-    </SlickContainer>
-  )
+  return <>MAIN</>
 }
 
 export default MainPage
