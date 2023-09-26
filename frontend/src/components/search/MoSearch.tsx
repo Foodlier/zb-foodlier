f3570bee17da54bb235b950341650b1cfd34f17e
seonohmin <seon.o.min@gmail.com>
2023-09-26 23:37:50 +0900

    검색, 배너 UI 구현

---
+++ frontend/src/components/search/MoSearch.tsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import styled from 'styled-components'
+import SearchBar from './SearchBar'
+import useIcon from '../../hooks/useIcon'
+import { breakpoints, palette } from '../../constants/Styles'
+
+interface MoSearchProps {
+  setIsMoSearchOpen: (isOpen: boolean) => void
+}
+
+const MoSearchContainer = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${palette.white};
+  z-index: 100;
+  overflow-y: scroll;
+
+  ${breakpoints.large} {
+    display: none;
+  }
+`
+
+const SearchHeader = styled.section`
+  display: flex;
+  width: 100%;
+  height: 6rem;
+  box-shadow: 0.1rem 0.5rem 1.1rem ${palette.shadow};
+  margin-bottom: 5rem;
+`
+
+const CloseButton = styled.button`
+  padding: 0rem 2rem;
+  border: none;
+  cursor: pointer;
+`
+
+const SearchHeaderTxt = styled.div`
+  display: flex;
+  position: relative;
+  left: 50%;
+  transform: translate(-25%);
+  width: 100%;
+  align-items: center;
+  font-size: 2rem;
+
+  & > p {
+    font-weight: 600;
+  }
+`
+
+const MoSearch: React.FC<MoSearchProps> = ({ setIsMoSearchOpen }) => {
+  const { IcPrevLight } = useIcon()
+
+  const handleSearch = (query: string) => {
+    console.log(`MoSearch에서 검색: ${query}`)
+  }
+
+  const closeMoSearch = () => {
+    setIsMoSearchOpen(false)
+  }
+
+  return (
+    <MoSearchContainer>
+      <SearchHeader>
+        <CloseButton type="button" onClick={closeMoSearch}>
+          <IcPrevLight size={5} color={palette.textPrimary} />
+        </CloseButton>
+        <SearchHeaderTxt>
+          <p>검색</p>
+        </SearchHeaderTxt>
+      </SearchHeader>
+      <SearchBar onSearch={handleSearch} />
+    </MoSearchContainer>
+  )
+}
+
+export default MoSearch
--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,4 +1,79 @@
+import React from 'react'
+import styled from 'styled-components'
+import SlickSlider from '../components/slider/Slider'
+import { slides } from '../components/slider/Slide'
+import { breakpoints } from '../constants/Styles'
+import WebSearch from '../components/search/WebSearch'
+import RecipeItem from '../components/recipe/RecipeItem'
+import Header from '../components/Header'
+
+const SlickContainer = styled.section`
+  margin-top: 3rem;
+`
+
+const Tit = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 800;
+  padding: 0 5%;
+
+  ${breakpoints.large} {
+    font-size: 3rem;
+  }
+`
+
+const WebSearchWrapper = styled.div`
+  display: none;
+
+  ${breakpoints.large} {
+    display: block;
+  }
+`
+
+const WebSearchContainer = styled.div`
+  padding: 0 5%;
+`
+
+const RecipeContainer = styled.section``
+const RecipeTit = styled.section``
+const RecipeList = styled.section`
+  flex-direction: column;
+  display: flex;
+
+  ${breakpoints.large} {
+    flex-direction: row;
+  }
+`
+
 const MainPage = () => {
-  return <>MAIN</>
+  const handleSearch = (query: string) => {
+    console.log(`WebSearch에서 검색: ${query}`)
+  }
+  return (
+    <>
+      {/* 헤더 */}
+      <Header />
+      {/* 검색 */}
+      <WebSearchWrapper>
+        <WebSearchContainer>
+          <WebSearch onSearch={handleSearch} />
+        </WebSearchContainer>
+      </WebSearchWrapper>
+      {/* 배너 */}
+      <SlickContainer>
+        <Tit>오늘 이 요리 어때요?</Tit>
+        <SlickSlider slides={slides} />
+      </SlickContainer>
+      {/* 레시피 */}
+      <RecipeContainer>
+        <RecipeTit>
+          <Tit>꿀조합 레시피</Tit>
+        </RecipeTit>
+        <RecipeList>
+          <RecipeItem />
+        </RecipeList>
+      </RecipeContainer>
+    </>
+  )
 }
+
 export default MainPage
