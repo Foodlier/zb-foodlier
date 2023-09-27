67c7949a3c3b6e8d5650433aa720e14eb522bf48
SeungHyunLee054 <yellow13lsh@naver.com>
2023-09-27 20:12:11 +0900

    Refactor: 요청서의 재료에 생성자 lombk 추가, dm 방 생성에 인자 추가
    #66

--- src/main/java/com/zerobase/foodlier/module/request/domain/vo/Ingredient.java
+++ src/main/java/com/zerobase/foodlier/module/request/domain/vo/Ingredient.java
@@ -1,12 +1,16 @@
 package com.zerobase.foodlier.module.request.domain.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ingredient {
     @Column(nullable = false)
     private String ingredientName;
