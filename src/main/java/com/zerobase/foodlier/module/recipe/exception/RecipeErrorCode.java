b71c02569ee1c2f89a34ab169ba99b657b6bba40
gomljo <gomljo0307@naver.com>
2023-09-29 18:53:44 +0900

    refactor: 레시피 검색 객체 수정 로직 변경
    변경 사항
    - 레시피 검색 객체의 수정 시 레시피 검색 객체에서 레시피 정보를 수정
    
    resolves: #64

--- src/main/java/com/zerobase/foodlier/module/recipe/exception/RecipeErrorCode.java
+++ src/main/java/com/zerobase/foodlier/module/recipe/exception/RecipeErrorCode.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RecipeErrorCode {
     NO_SUCH_RECIPE("레시피가 없습니다."),
+    NO_SUCH_RECIPE_DOCUMENT("레시피 검색 객체를 찾을 수 없습니다."),
     NO_PERMISSION("레시피 접근 권한이 없습니다."),
     ;
 
