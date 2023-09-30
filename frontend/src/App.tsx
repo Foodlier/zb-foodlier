6be3272b25eff9fd94f0c24ce30159063195237b
d-bCODING <rlaehq@gmail.com>
2023-10-01 02:21:31 +0900

    Docs : 임의설정 Route 삭제
    개발 과정 중에 임의로 설정했던 Route 삭제입니다

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,6 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import CookForMe from './pages/refrigerator/CookForMe'
 import MainPage from './pages/MainPage'
-import RequestDetail from './pages/refrigerator/RequestDetail'
-import Request from './pages/refrigerator/Request'
-import CookForYou from './pages/refrigerator/CookForYou'
-import WriteRecipePage from './pages/recipe/WriteRecipePage'
-import WriteEstimatePage from './pages/refrigerator/WriteEstimatePage'
-import EstimateDetail from './pages/refrigerator/EstimateDetail'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -25,12 +19,6 @@ function App() {
       <Route path="/main" element={<MainPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/cook-for-me" element={<CookForMe />} />
-      <Route path="/cook-for-you" element={<CookForYou />} />
-      <Route path="/request" element={<Request />} />
-      <Route path="/request-detail" element={<RequestDetail />} />
-      <Route path="/a" element={<WriteRecipePage />} />
-      <Route path="/b" element={<WriteEstimatePage />} />
-      <Route path="/c" element={<EstimateDetail />} />
     </Routes>
   )
 }
