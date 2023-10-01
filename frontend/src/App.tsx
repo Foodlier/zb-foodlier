f645245a1070cacdb0ff58512777352703c15e2d
minji-yun <minpaper.dev@gmail.com>
2023-10-01 22:11:32 +0900

    Fix: router Main 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 import CookForMe from './pages/CookForMe'
 import MainPage from './pages/MainPage'
+import RecipePage from './pages/recipe/RecipePage'
+import WriteRecipePage from './pages/recipe/WriteRecipePage'
+import ChattingPage from './pages/chat/ChattingPage'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -15,10 +18,13 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="/main" element={<MainPage />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/cook-for-me" element={<CookForMe />} />
+      <Route path="/recipe" element={<RecipePage />} />
+      <Route path="/recipe/write" element={<WriteRecipePage />} />
+      <Route path="/chat" element={<ChattingPage />} />
     </Routes>
   )
 }
