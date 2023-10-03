ed7315c669204726fbb8adf651cff6418a193817
SeungHyunLee054 <yellow13lsh@naver.com>
2023-10-03 14:29:04 +0900

    Feature: 좋아요 누르기/취소
    ifPresentOrElse를 통해 recipeId를 통해 HeartRepository에서 조회한 후 만약 존재한다면 좋아요가 false일때 해당 true로 바꾸고 좋아요 수를 증가함 이미 true라면 오류 thorw, 아에 존재하지 않는 경우 recipe와 member를 조회해서 새로 생성
    취소일 경우는 존재한다면 true일때 false로 변경, 이미 false라면 오류 throw, 존재 하지 않을 경우도 오류 throw
    
    #100

--- src/main/java/com/zerobase/foodlier/module/heart/reposiotry/HeartRepository.java
+++ src/main/java/com/zerobase/foodlier/module/heart/reposiotry/HeartRepository.java
@@ -3,5 +3,8 @@ package com.zerobase.foodlier.module.heart.reposiotry;
 import com.zerobase.foodlier.module.heart.domain.model.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HeartRepository extends JpaRepository<Heart, Long> {
+    Optional<Heart> findByRecipeId(Long recipeId);
 }
