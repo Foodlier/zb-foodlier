6818f63d7fd07095dcc99675ce3dda2b20167cd7
gomljo <gomljo0307@naver.com>
2023-10-03 19:28:33 +0900

    feat: 알림 도메인 정의 및 repository 구현
    - 특정 요청 이후 사용자에게 알림을 전송할 때 필요한 알림 도메인 정의
    - 알림 도메인에 대한 CRUD를 담당하는 repository 정의
    
    resolves: #102

--- src/main/java/com/zerobase/foodlier/module/notification/service/NotificationService.java
+++ src/main/java/com/zerobase/foodlier/module/notification/service/NotificationService.java
@@ -1,7 +1,17 @@
 package com.zerobase.foodlier.module.notification.service;
 
-import org.springframework.stereotype.Service;
+import com.zerobase.foodlier.module.member.member.domain.model.Member;
+import com.zerobase.foodlier.module.notification.domain.model.Notification;
+import com.zerobase.foodlier.module.notification.domain.type.NotificationType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-@Service
+import java.util.List;
+
 public interface NotificationService {
+    Notification create(Member receiver, NotificationType notificationType, String content, String url);
+
+    List<Notification> getSimpleNotification(Long id);
+
+    Page<Notification> getNotificationBy(Long id, Pageable pageable);
 }
