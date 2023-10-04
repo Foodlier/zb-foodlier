86b455fb2cd70dafe7475ad104086a3b238f8e12
J-HyeonSeo <jhsfully@gmail.com>
2023-10-04 12:26:46 +0900

    Feat: RecipeService에 별점 조작 로직 구현 및 Redis Locking 어노테이션 추가

--- src/main/java/com/zerobase/foodlier/module/recipe/domain/vo/RecipeStatistics.java
+++ src/main/java/com/zerobase/foodlier/module/recipe/domain/vo/RecipeStatistics.java
@@ -17,4 +17,40 @@ public class RecipeStatistics {
     private int reviewCount;
     private int reviewStarSum;
     private double reviewStarAverage;
+
+    public void plusStar(int star){
+        this.reviewCount++;
+        this.reviewStarSum += star;
+        this.calcStarAvg();
+    }
+
+    public void updateStar(int originStar, int newStar){
+        this.reviewStarSum -= originStar;
+
+        if(this.reviewStarSum < 0){
+            this.reviewStarSum = 0;
+        }
+
+        this.reviewStarSum += newStar;
+        this.calcStarAvg();
+    }
+
+    public void minusStar(int star){
+        this.reviewCount--;
+        this.reviewStarSum -= star;
+
+        if(this.reviewStarSum < 0){
+            this.reviewStarSum = 0;
+        }
+        this.calcStarAvg();
+    }
+
+    private void calcStarAvg(){
+        if(this.reviewCount <= 0){
+            this.reviewStarAverage = 0;
+            return;
+        }
+        this.reviewStarAverage = (double) reviewStarSum / reviewCount;
+    }
+
 }
