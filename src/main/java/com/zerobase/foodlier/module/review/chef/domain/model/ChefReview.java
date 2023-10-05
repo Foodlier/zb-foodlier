424535f4683f2da373f96f9783e0d8fd5616ffdd
J-HyeonSeo <jhsfully@gmail.com>
2023-10-05 21:21:48 +0900

    Fix: ChefReview테이블 칼럼 변경
    - title칼럼 제거
    - grade -> star 변경
    - cookUrl 칼럼 제거

--- src/main/java/com/zerobase/foodlier/module/review/chef/domain/model/ChefReview.java
+++ src/main/java/com/zerobase/foodlier/module/review/chef/domain/model/ChefReview.java
@@ -18,12 +18,9 @@ public class ChefReview extends Audit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(nullable = false)
-    private String title;
-    @Column(nullable = false)
     private String content;
     @Column(nullable = false)
-    private Integer grade;
-    private String cookUrl;
+    private int star;
 
     @OneToOne
     private Request request;
