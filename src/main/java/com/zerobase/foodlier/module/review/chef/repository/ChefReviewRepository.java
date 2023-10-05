2ae2934ba5a7366516ae32d2bf2180aa10b08b54
J-HyeonSeo <jhsfully@gmail.com>
2023-10-05 21:23:18 +0900

    Feat: ChefReviewServiceImpl 기능 구현
    - 요리사 리뷰 작성
    - 요리사 리뷰 조회

--- src/main/java/com/zerobase/foodlier/module/review/chef/repository/ChefReviewRepository.java
+++ src/main/java/com/zerobase/foodlier/module/review/chef/repository/ChefReviewRepository.java
@@ -1,7 +1,15 @@
 package com.zerobase.foodlier.module.review.chef.repository;
 
+import com.zerobase.foodlier.module.member.chef.domain.model.ChefMember;
+import com.zerobase.foodlier.module.request.domain.model.Request;
 import com.zerobase.foodlier.module.review.chef.domain.model.ChefReview;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChefReviewRepository extends JpaRepository<ChefReview, Long> {
+
+    boolean existsByRequest(Request request);
+    Page<ChefReview> findByChefMember(ChefMember chefMember, Pageable pageable);
+
 }
