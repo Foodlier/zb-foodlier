e64ccc5b5a211dd0153a7c458d2f5343d8a955f2
Boseong Choi <bos33210@naver.com>
2023-10-06 13:37:04 +0900

    Feat: 회원가입 MSW 최종 적용
    - API 명세서 기준으로 POST 메서드 응답 구축
    - MSW 세팅 구현 완료

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-// import { worker } from './mocks/worker'
-
 import App from './App'
-// import './index.css'
 import './reset.css'
+import { worker } from './mocks/browsers'
 
-// if (process.env.NODE_ENV === 'development') {
-//   worker.start()
-// }
+if (process.env.NODE_ENV === 'development') {
+  worker.start()
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
