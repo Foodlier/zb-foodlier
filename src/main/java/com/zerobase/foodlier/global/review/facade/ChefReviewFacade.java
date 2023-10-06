f5886937e5a9624a4a50069d9490fc4376b6edc5
J-HyeonSeo <jhsfully@gmail.com>
2023-10-06 09:29:22 +0900

    Comment: ChefReview 메서드에 대한 주석설명 추가

--- src/main/java/com/zerobase/foodlier/global/review/facade/ChefReviewFacade.java
+++ src/main/java/com/zerobase/foodlier/global/review/facade/ChefReviewFacade.java
@@ -16,6 +16,11 @@ public class ChefReviewFacade {
     private final ChefReviewService chefReviewService;
     private final ChefMemberService chefMemberService;
 
+    /**
+     *  작성자 : 전현서
+     *  작성일 : 2023-10-06
+     *  요리사에 대한 후기를 작성하고, ChefMember의 exp와 star를 올려줌.
+     */
     public void createChefReview(Long memberId, Long requestId, ChefReviewForm form){
 
         Long chefMemberId = chefReviewService.createChefReview(memberId, requestId, form);
