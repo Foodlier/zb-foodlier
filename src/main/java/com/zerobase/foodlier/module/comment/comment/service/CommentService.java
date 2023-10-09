b23bb2b6241c8d2c849ef56cd54ef32737567e44
gomljo <gomljo0307@naver.com>
2023-10-09 15:12:52 +0900

    feat: 댓글 CRUD 구현
    구현 목록
    
    - 댓글 생성 기능 구현
    - 댓글 목록 조회 기능 구현
    - 댓글 조회 기능 구현
    - 댓글 삭제 기능 구현
    - 댓글 수정 기능 구현
    
    resolves: #101

--- src/main/java/com/zerobase/foodlier/module/comment/comment/service/CommentService.java
+++ src/main/java/com/zerobase/foodlier/module/comment/comment/service/CommentService.java
@@ -1,7 +1,15 @@
 package com.zerobase.foodlier.module.comment.comment.service;
 
+import com.zerobase.foodlier.module.comment.comment.domain.model.Comment;
+import com.zerobase.foodlier.module.comment.comment.dto.CommentPagingDto;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface CommentService {
+    void createComment(Comment comment);
+    void updateComment(Long memberId, Long commentId, String modifiedMessage);
+    void deleteComment(Long memberId, Long commentId);
+    CommentPagingDto getCommentList(Long recipeId, PageRequest pageRequest);
+    Comment findComment(Long commentId);
 }
