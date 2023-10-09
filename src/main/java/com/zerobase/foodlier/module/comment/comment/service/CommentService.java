d37829c3b0dfcf7831ea5ad34cd3fafcb2e60126
gomljo <gomljo0307@naver.com>
2023-10-09 16:56:33 +0900

    chore: 불필요한 어노테이션 제거
    CommentService 인터페이스에 존재하는 불필요한 어노테이션 제거
    
    resolves: #101

--- src/main/java/com/zerobase/foodlier/module/comment/comment/service/CommentService.java
+++ src/main/java/com/zerobase/foodlier/module/comment/comment/service/CommentService.java
@@ -3,9 +3,7 @@ package com.zerobase.foodlier.module.comment.comment.service;
 import com.zerobase.foodlier.module.comment.comment.domain.model.Comment;
 import com.zerobase.foodlier.module.comment.comment.dto.CommentPagingDto;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface CommentService {
     void createComment(Comment comment);
     void updateComment(Long memberId, Long commentId, String modifiedMessage);
