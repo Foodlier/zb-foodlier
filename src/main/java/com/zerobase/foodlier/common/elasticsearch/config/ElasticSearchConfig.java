04f46134a0cb571e26758afeedf27e0618713b42
gomljo <gomljo0307@naver.com>
2023-10-09 21:14:22 +0900

    refactor: elasticsearch 설정 변경
    개발 환경에서 사용하기 위한 elastic search 설정 변경
    
    resolves: #189

--- src/main/java/com/zerobase/foodlier/common/elasticsearch/config/ElasticSearchConfig.java
+++ src/main/java/com/zerobase/foodlier/common/elasticsearch/config/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.zerobase.foodlier.common.elasticsearch.config;
 
 import com.zerobase.foodlier.module.recipe.repository.RecipeSearchRepository;
+import lombok.RequiredArgsConstructor;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,33 +14,33 @@ import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
+@RequiredArgsConstructor
 @EnableElasticsearchRepositories(basePackageClasses = RecipeSearchRepository.class)
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
-    @Value("${elasticsearch.host}")
-    private String host;
+    @Value("${elasticsearch.connection}")
+    private String connection;
 
-    @Value("${elasticsearch.port}")
-    private String port;
-
-    @Value("${elasticsearch.username}")
+    @Value("${elasticsearch.username:#{null}}")
     private String username;
 
-    @Value("${elasticsearch.password}")
+    @Value("${elasticsearch.password:#{null}}")
     private String password;
 
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
+
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo(host+":" + port)
-                .withBasicAuth(username, password)
-                .build();
+                        .connectedTo(connection)
+                        .withBasicAuth(username, password)
+                        .build();
+
         return RestClients.create(clientConfiguration).rest();
     }
 
     @Bean
-    public ElasticsearchOperations elasticsearchOperations(){
+    public ElasticsearchOperations elasticsearchOperations() {
         return new ElasticsearchRestTemplate(elasticsearchClient());
     }
 }
