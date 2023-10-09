7966cd22de9c465d861b88b2996879d5083433d1
gomljo <gomljo0307@naver.com>
2023-10-09 15:20:46 +0900

    feat: 댓글, 대댓글 facade 서비스 구현
    댓글과 대댓글 작성 및 삭제 시 레시피의 댓글 수에 대한 동시성 제어가 필요
    따라서 댓글과 대댓글 작성 및 삭제 시 레시피와 함께 lock을 사용하도록
    구현
    
    resolves: #101

---
+++ src/main/java/com/zerobase/foodlier/global/comment/facade/reply/ReplyFacade.java
@@ -0,0 +1,48 @@
+package com.zerobase.foodlier.global.comment.facade.reply;
+
+import com.zerobase.foodlier.common.aop.RedissonLock;
+import com.zerobase.foodlier.module.comment.comment.domain.model.Comment;
+import com.zerobase.foodlier.module.comment.comment.service.CommentService;
+import com.zerobase.foodlier.module.comment.reply.domain.model.Reply;
+import com.zerobase.foodlier.module.comment.reply.servcie.ReplyService;
+import com.zerobase.foodlier.module.member.member.domain.model.Member;
+import com.zerobase.foodlier.module.member.member.service.MemberService;
+import com.zerobase.foodlier.module.recipe.domain.model.Recipe;
+import com.zerobase.foodlier.module.recipe.service.recipe.RecipeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReplyFacade {
+
+    private final ReplyService replyService;
+    private final CommentService commentService;
+    private final MemberService memberService;
+    private final RecipeService recipeService;
+
+    @Transactional
+    @RedissonLock(group = "reply", key = "#recipeId")
+    public void createReply(Long commentId, Long recipeId, String userEmail, String message) {
+
+        Member member = memberService.findByEmail(userEmail);
+
+        Recipe recipe = recipeService.plusCommentCount(recipeId);
+        Comment comment = commentService.findComment(commentId);
+        replyService.createReply(Reply.builder()
+                .message(message)
+                .comment(comment)
+                .member(member)
+                .build());
+    }
+
+    @Transactional
+    @RedissonLock(group = "reply", key = "#recipeId")
+    public void deleteReply(Long replyId, Long recipeId, Long memberId) {
+        recipeService.minusCommentCount(recipeId);
+        replyService.deleteReply(memberId,replyId);
+    }
+
+
+}
