c846237e63314f3d6cb6d68bb0fd59d4b247e503
gomljo <gomljo0307@naver.com>
2023-10-09 15:15:42 +0900

    feat: 대댓글 domain 및 repository 정의
    대댓글 domain
    - 대댓글 수정 기능 구현
    
    대댓글 repository
    - 대댓글 조회하기 구현
    - 레시피에 작성된 대댓글 목록 조회하기 구현
    
    resolves: #101

--- src/main/java/com/zerobase/foodlier/module/comment/reply/reposiotry/ReplyRepository.java
+++ src/main/java/com/zerobase/foodlier/module/comment/reply/reposiotry/ReplyRepository.java
@@ -1,8 +1,29 @@
 package com.zerobase.foodlier.module.comment.reply.reposiotry;
 
 import com.zerobase.foodlier.module.comment.reply.domain.model.Reply;
+import com.zerobase.foodlier.module.comment.reply.dto.ReplyDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    @Query("select r " +
+            "from Reply r " +
+            "left join r.member m " +
+            "where m.id = :memberId " +
+            "and r.id = :replyId")
+    Optional<Reply> findReply(@Param("replyId") Long replyId,
+                              @Param("memberId") Long memberId);
+
+    @Query("select r " +
+            "from Reply r " +
+            "join r.comment c " +
+            "where c.id = :commentId")
+    Page<ReplyDto> findReplyList(@Param("commentId") Long commentId,
+                                 Pageable pageable);
 }
