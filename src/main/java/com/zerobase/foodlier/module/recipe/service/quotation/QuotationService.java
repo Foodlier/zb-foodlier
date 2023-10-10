c071d8d89d002cb6581cd48e047702f275c72780
J-HyeonSeo <jcs2349@gmail.com>
2023-10-10 21:32:47 +0900

    Fix: quotation과 연관된 List응답 수정

--- src/main/java/com/zerobase/foodlier/module/recipe/service/quotation/QuotationService.java
+++ src/main/java/com/zerobase/foodlier/module/recipe/service/quotation/QuotationService.java
@@ -1,5 +1,6 @@
 package com.zerobase.foodlier.module.recipe.service.quotation;
 
+import com.zerobase.foodlier.common.response.ListResponse;
 import com.zerobase.foodlier.module.recipe.domain.model.Recipe;
 import com.zerobase.foodlier.module.recipe.dto.quotation.QuotationDetailResponse;
 import com.zerobase.foodlier.module.recipe.dto.quotation.QuotationDtoRequest;
@@ -7,12 +8,10 @@ import com.zerobase.foodlier.module.recipe.dto.quotation.QuotationTopResponse;
 import com.zerobase.foodlier.module.recipe.dto.recipe.RecipeDtoRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
-
 public interface QuotationService {
     void createQuotation(Long memberId, QuotationDtoRequest request);
-    List<QuotationTopResponse> getQuotationListForRefrigerator(Long memberId, Pageable pageable);
-    List<QuotationTopResponse> getQuotationListForRecipe(Long memberId, Pageable pageable);
+    ListResponse<QuotationTopResponse> getQuotationListForRefrigerator(Long memberId, Pageable pageable);
+    ListResponse<QuotationTopResponse> getQuotationListForRecipe(Long memberId, Pageable pageable);
     QuotationDetailResponse getQuotationDetail(Long memberId, Long quotationId);
     void convertToRecipe(Long memberId, Long quotationId, RecipeDtoRequest request);
     void updateQuotation(Long memberId, Long quotationId, QuotationDtoRequest request);
