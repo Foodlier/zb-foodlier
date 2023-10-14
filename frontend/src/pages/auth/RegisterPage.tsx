79e79594ece04721976916ce101b7c8c6f86ed5c
Boseong Choi <bos33210@naver.com>
2023-10-14 15:40:51 +0900

    Feat: 회원가입 구현 완료
    MSW 대신 AWS EC2로 API 테스트 결과 이상없습니다.
    
    추후 리팩토링 시 기능 추가 예정입니다.

--- frontend/src/pages/auth/RegisterPage.tsx
+++ frontend/src/pages/auth/RegisterPage.tsx
@@ -1,46 +1,49 @@
 import { ChangeEvent, useState, useRef, useEffect } from 'react'
 import DaumPostcode from 'react-daum-postcode'
-import { rest } from 'msw'
+// import { rest } from 'msw'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import * as S from '../../styles/auth/RegisterPage.styled'
 import upload from '../../../public/images/profile_upload.svg'
 import Modal from '../../components/auth/AddressModal'
-import { worker } from '../../mocks/browsers'
+// import { worker } from '../../mocks/browsers'
 
 interface FormData {
   profileImage: File | null
   nickname: string
   email: string
   verificationCode: string
+  phoneNumber: string
   password: string
   passwordCheck: string
-  address: string
-  detailAddress: string
+  roadAddress: string
+  addressDetail: string
 }
 
 interface FormErrors {
   nickname: string
   email: string
   verificationCode: string
+  phoneNumber: string
   password: string
   passwordCheck: string
-  address: string
-  detailAddress: string
+  roadAddress: string
+  addressDetail: string
 }
 
 const Register = () => {
   const navigate = useNavigate()
   // 폼 데이터 관련 상태
   const [formData, setFormData] = useState<FormData>({
-    profileImage: null as File | null,
+    profileImage: null,
     nickname: '',
     email: '',
     verificationCode: '',
+    phoneNumber: '',
     password: '',
     passwordCheck: '',
-    address: '',
-    detailAddress: '',
+    roadAddress: '',
+    addressDetail: '',
   })
 
   // 폼 유효성 검사 관련 상태
@@ -48,24 +51,23 @@ const Register = () => {
     nickname: '',
     email: '',
     verificationCode: '',
+    phoneNumber: '',
     password: '',
     passwordCheck: '',
-    address: '',
-    detailAddress: '',
+    roadAddress: '',
+    addressDetail: '',
   })
 
   const [isCodeInput, setIsCodeInput] = useState<boolean>(false)
   const [verificationCode, setVerificationCode] = useState<string>('')
 
+  // TODO : 인증번호 전송 API 호출 후 로딩바 또는 로딩 텍스트 표시
   const sendVerificationEmail = async () => {
-    const endpoint = `/auth/verification/send/${formData.email}`
     setIsCodeInput(true)
-
-    axios.post(endpoint).then(res => {
-      if (res.data) {
-        console.log(res.data.code)
-        setVerificationCode(res.data.code)
-      }
+    // API : 인증번호 전송 요청
+    axios.post(`/api/auth/verification/send/${formData.email}`).then(res => {
+      alert('인증번호가 전송되었습니다.')
+      // console.log(res)
     })
   }
 
@@ -105,7 +107,7 @@ const Register = () => {
       setAddress(data.address)
       setOpenPostcode(false)
       setIsModalOpen(false)
-      setFormData({ ...formData, address: data.address })
+      setFormData({ ...formData, roadAddress: data.address })
     },
   }
   const openModal = () => {
@@ -116,10 +118,7 @@ const Register = () => {
     setIsModalOpen(false)
   }
 
-  /*
-    이미지 업로드 관련 상태, 이벤트
-  */
-  // const [imgFile, setImgFile] = useState<File | null>(null)
+  // 이미지 업로드 관련 상태, 이벤트
   const imgRef = useRef<HTMLInputElement | null>(null)
 
   const handleImgFile = (e: ChangeEvent<HTMLInputElement>) => {
@@ -130,13 +129,13 @@ const Register = () => {
       setFormData({ ...formData, profileImage: e.target.files[0] })
     }
   }
-  /*
-    폼 입력값 변경 이벤트 및 유효성 검사
-   */
+
+  // 폼 입력값 변경 이벤트 및 유효성 검사
+  // TODO : React 18 -> useDeferredValue 등 사용하여 성능 최적화
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target
     setFormData({ ...formData, [name]: value })
-    // console.log(formData)
+    setFormErrors({ ...formErrors, [name]: '' })
 
     // 입력값 유효성 검사
     switch (name) {
@@ -157,10 +156,23 @@ const Register = () => {
         }
         break
       case 'verificationCode':
-        if (value != verificationCode) {
+        if (value === '') {
           setFormErrors({
             ...formErrors,
-            [name]: '인증번호가 일치하지 않습니다.',
+            [name]: '인증번호를 입력해주세요.',
+          })
+        } else {
+          setFormErrors({
+            ...formErrors,
+            [name]: '',
+          })
+        }
+        break
+      case 'phoneNumber':
+        if (value === '') {
+          setFormErrors({
+            ...formErrors,
+            [name]: '휴대폰 번호를 입력해주세요.',
           })
         } else {
           setFormErrors({
@@ -174,6 +186,11 @@ const Register = () => {
           setFormErrors({
             ...formErrors,
             [name]: '4자리 이상 입력해주세요.',
+          })
+        } else if (value !== formData.passwordCheck) {
+          setFormErrors({
+            ...formErrors,
+            [name]: '비밀번호가 일치하지 않습니다.',
           })
         } else {
           setFormErrors({
@@ -195,7 +212,7 @@ const Register = () => {
           })
         }
         break
-      case 'address':
+      case 'roadAddress':
         if (value === '') {
           setFormErrors({
             ...formErrors,
@@ -203,7 +220,7 @@ const Register = () => {
           })
         }
         break
-      case 'detailAddress':
+      case 'addressDetail':
         if (value === '') {
           setFormErrors({
             ...formErrors,
@@ -216,6 +233,29 @@ const Register = () => {
     }
   }
 
+  const handleVerificationCode = () => {
+    // API : 확인 버튼 누르면 인증번호 일치 여부 확인
+    axios
+      .post(
+        `/api/auth/verify`,
+        {},
+        {
+          params: {
+            email: formData.email,
+            verificationCode: formData.verificationCode,
+          },
+        }
+      )
+      .then(function () {
+        alert('인증이 완료되었습니다.')
+        // console.log(response)
+      })
+      .catch(function () {
+        alert('인증번호가 일치하지 않습니다.')
+        // console.log(error)
+      })
+  }
+
   // 입력값 여부에 따라 버튼 활성화 상태 변경
   useEffect(() => {
     const {
@@ -224,8 +264,8 @@ const Register = () => {
       verificationCode,
       password,
       passwordCheck,
-      address,
-      detailAddress,
+      roadAddress,
+      addressDetail,
     } = formData
 
     if (
@@ -234,8 +274,8 @@ const Register = () => {
       verificationCode &&
       password &&
       passwordCheck &&
-      address &&
-      detailAddress
+      roadAddress &&
+      addressDetail
     ) {
       setIsFormValid(true)
     } else {
@@ -246,59 +286,40 @@ const Register = () => {
   //  회원가입 폼 제출 이벤트
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
-    try {
-      const endpoint = '/api/auth/signup'
 
-      const formDataToSend = new FormData()
+    const endpoint = '/api/auth/signup'
+
+    const formDataToSend = new FormData()
+    formDataToSend.append('addressDetail', formData.addressDetail)
+    formDataToSend.append('email', formData.email)
+    formDataToSend.append('nickname', formData.nickname)
+    formDataToSend.append('password', formData.password)
+    formDataToSend.append('phoneNumber', formData.phoneNumber)
+    // 이미지 파일이 없을 경우 FormData에서 삭제 후 백엔드에 null 타입으로 전송
+    if (formData.profileImage === null) {
+      formDataToSend.append('profileImage', '')
+      formDataToSend.delete('profileImage')
+    } else {
       formDataToSend.append('profileImage', formData.profileImage as Blob)
-      formDataToSend.append('nickname', formData.nickname)
-      formDataToSend.append('email', formData.email)
-      formDataToSend.append('password', formData.password)
-      formDataToSend.append('address', formData.address)
-      formDataToSend.append('detailAddress', formData.detailAddress)
+    }
+    formDataToSend.append('address', formData.roadAddress)
 
-      worker.use(
-        rest.post(endpoint, (_req, res, ctx) => {
-          return res(ctx.status(200), ctx.json({ message: '회원가입 성공' }))
-        })
-      )
-
-      // eslint-disable-next-line no-restricted-syntax
-      // for (const key of formDataToSend.keys()) {
-      //   console.log(key)
-      // }
-      // console.log('====================================')
-      // // eslint-disable-next-line no-restricted-syntax
-      // for (const value of formDataToSend.values()) {
-      //   console.log(value)
-      // }
-
-      // axios를 사용하여 POST 요청을 보냅니다.
-      // const response = await axios.post(endpoint, formData, {
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      // })
-      // Axios 대신 fetch 또는 다른 HTTP 클라이언트를 사용하여 요청을 보낼 수 있습니다.
-      const response = await fetch(endpoint, {
-        method: 'POST',
-        body: JSON.stringify(formDataToSend),
+    // API : 회원가입 요청
+    axios
+      .post(endpoint, formDataToSend, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       })
-
-      // HTTP 응답 코드 및 데이터를 확인합니다.
-      if (response.status === 200) {
-        console.log('회원가입 성공:', await response.json())
-        alert('회원가입 성공')
-        navigate('/')
-      } else {
-        console.error('회원가입 실패:', await response.json())
-      }
-    } catch (error) {
-      console.error('회원가입 실패:', error)
-    }
+      .then(res => {
+        console.log(res)
+        alert('회원가입이 완료되었습니다.')
+        navigate('/login')
+      })
+      .catch(err => {
+        console.log(err)
+        alert('회원가입에 실패하였습니다.')
+      })
   }
 
   return (
@@ -366,17 +387,32 @@ const Register = () => {
             </S.DetailButton>
             <S.ErrorMessage>{formErrors.email}</S.ErrorMessage>
             {isCodeInput ? (
-              <S.Input1
-                type="text"
-                name="verificationCode"
-                placeholder="인증번호를 입력해주세요"
-                value={formData.verificationCode}
-                onChange={handleChange}
-              />
+              <>
+                <S.Input2
+                  type="text"
+                  name="verificationCode"
+                  placeholder="인증번호를 입력해주세요"
+                  value={formData.verificationCode}
+                  onChange={handleChange}
+                />
+                <S.DetailButton type="button" onClick={handleVerificationCode}>
+                  확인
+                </S.DetailButton>
+              </>
             ) : (
               <div />
             )}
             <S.ErrorMessage>{formErrors.verificationCode}</S.ErrorMessage>
+            <S.InputContainer>
+              <S.Input1
+                type="string"
+                name="phoneNumber"
+                placeholder="휴대폰 번호"
+                value={formData.phoneNumber}
+                onChange={handleChange}
+              />
+              <S.ErrorMessage>{formErrors.phoneNumber}</S.ErrorMessage>
+            </S.InputContainer>
           </S.InputContainer>
           <S.InputContainer>
             <S.Input1
@@ -402,23 +438,23 @@ const Register = () => {
             <S.Input2
               type="string"
               disabled
-              value={formData.address}
+              value={formData.roadAddress}
               name="address"
               onChange={handleChange}
             />
             <S.DetailButton type="button" onClick={openModal}>
               주소검색
             </S.DetailButton>
-            <S.ErrorMessage>{formErrors.address}</S.ErrorMessage>
+            <S.ErrorMessage>{formErrors.roadAddress}</S.ErrorMessage>
           </S.InputContainer>
           <S.Input1
             type="string"
-            name="detailAddress"
+            name="addressDetail"
             placeholder="상세 주소"
-            value={formData.detailAddress}
+            value={formData.addressDetail}
             onChange={handleChange}
           />
-          <S.ErrorMessage>{formErrors.detailAddress}</S.ErrorMessage>
+          <S.ErrorMessage>{formErrors.addressDetail}</S.ErrorMessage>
           {isFormValid ? (
             <S.ConfirmButton type="submit">가입하기</S.ConfirmButton>
           ) : (
