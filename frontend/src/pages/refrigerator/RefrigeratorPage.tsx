4963fec3b72f41eadab7e55f0e3f9792943178f9
mlnpaper <minpaper.dev@gmail.com>
2023-10-16 02:26:21 +0900

    Fix: 냉장고를 부탁해 지도 구현 및 component 분리

--- frontend/src/pages/refrigerator/RefrigeratorPage.tsx
+++ frontend/src/pages/refrigerator/RefrigeratorPage.tsx
@@ -1,63 +1,50 @@
-import { useEffect, useState } from 'react'
-import { Map } from 'react-kakao-maps-sdk'
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState } from 'react'
 import { useRecoilValue } from 'recoil'
+import * as S from '../../styles/refrigerator/RefrigeratorPage.styled'
 import Header from '../../components/Header'
 import BottomNavigation from '../../components/BottomNavigation'
-import * as S from '../../styles/refrigerator/RefrigeratorPage.styled'
 import CookForMe from '../../components/refrigerator/CookForMe'
 import CookForYou from '../../components/refrigerator/CookForYou'
+
 import { myInfoState } from '../../store/recoilState'
-import axiosInstance from '../../utils/FetchCall'
+import RefrigeratorMap from '../../components/refrigerator/RefrigeratorMap'
+import { MarkerItem } from '../../constants/Interfaces'
 
 const RefrigeratorPage = () => {
   const profile = useRecoilValue(myInfoState)
-  const [userType, setUserType] = useState('requester')
-
-  const pageIdx = 1
-  const pageSize = 20
-
-  const getChef = async () => {
-    try {
-      const res = await axiosInstance.get(
-        `/refrigerator/chef/${pageIdx}/${pageSize}`,
-        { params: { type: 'DISTANCE' } }
-      )
-      console.log(res)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    getChef()
-  }, [])
+  const [userType, setUserType] = useState('user')
+  const [mapMarkerList, setMapMarkerList] = useState<MarkerItem[]>([])
 
   return (
     <>
       <Header />
       <S.Container>
-        <S.Map>
-          <Map
-            center={{ lat: profile.address.lat, lng: profile.address.lnt }} // 지도의 중심 좌표
-            style={{ width: '100%', height: '100%' }} // 지도 크기
-            level={4}
-          />
-        </S.Map>
-        <S.SelectUserList>
-          <S.SelectTypeButton
-            type="button"
-            onClick={() => setUserType('requester')}
-          >
-            요청자
-          </S.SelectTypeButton>
-          <S.SelectTypeButton
-            type="button"
-            onClick={() => setUserType('cooker')}
-          >
-            요리사
-          </S.SelectTypeButton>
-        </S.SelectUserList>
-        {userType === 'requester' ? <CookForMe /> : <CookForYou />}
+        <RefrigeratorMap markerList={mapMarkerList} />
+        {profile?.isChef && (
+          <S.SelectUserList>
+            <S.SelectTypeButton
+              type="button"
+              onClick={() => setUserType('user')}
+              $isActive={userType === 'user'}
+            >
+              요청자
+            </S.SelectTypeButton>
+            <S.SelectTypeButton
+              type="button"
+              onClick={() => setUserType('chef')}
+              $isActive={userType === 'chef'}
+            >
+              요리사
+            </S.SelectTypeButton>
+          </S.SelectUserList>
+        )}
+
+        {userType === 'user' ? (
+          <CookForMe setMapMarkerList={setMapMarkerList} />
+        ) : (
+          <CookForYou setMapMarkerList={setMapMarkerList} />
+        )}
       </S.Container>
       <BottomNavigation />
     </>
