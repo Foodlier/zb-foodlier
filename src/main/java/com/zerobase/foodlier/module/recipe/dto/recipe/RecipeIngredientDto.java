6c4e1e840a1b4831563b826042920a6a8614434c
J-HyeonSeo <jcs2349@gmail.com>
2023-10-15 20:06:44 +0900

    Feat: Recipe 관련 Validation 추가

--- src/main/java/com/zerobase/foodlier/module/recipe/dto/recipe/RecipeIngredientDto.java
+++ src/main/java/com/zerobase/foodlier/module/recipe/dto/recipe/RecipeIngredientDto.java
@@ -3,14 +3,20 @@ package com.zerobase.foodlier.module.recipe.dto.recipe;
 import com.zerobase.foodlier.module.recipe.domain.vo.RecipeIngredient;
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class RecipeIngredientDto {
+    @NotBlank(message = "요리 재료를 입력해주세요")
     private String name;
+    @Min(value = 1, message = "갯수는 1이상으로 입력해주세요")
     private int count;
+    @NotBlank(message = "단위를 입력해주세요.")
     private String unit;
 
     public RecipeIngredient toEntity() {
