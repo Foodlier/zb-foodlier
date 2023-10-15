766670b59bcf481ea2ad53bed6c2c526efa33d8a
mlnpaper <minpaper.dev@gmail.com>
2023-10-16 02:33:41 +0900

    Feat: Empty 관련 component 생성

---
+++ frontend/src/components/ui/EmptyView.tsx
@@ -0,0 +1,7 @@
+import * as S from '../../styles/ui/EmptyView.styled'
+
+const EmptyView = ({ content }: { content: string }) => {
+  return <S.EmptyView>{content}</S.EmptyView>
+}
+
+export default EmptyView
