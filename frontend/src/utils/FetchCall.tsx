f5e3bbd4b2a13b1d125c81132a86b7d28785129b
mlnpaper <minpaper.dev@gmail.com>
2023-10-16 02:39:37 +0900

    Feat: refresh token으로 token 재발급 받는 통신 추가

--- frontend/src/utils/FetchCall.tsx
+++ frontend/src/utils/FetchCall.tsx
@@ -1,9 +1,10 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 
 const API_BASE_URL = 'http://localhost:8080'
 // 서버에서 받아온 안전한 accountToken 사용
 const API_TOKEN = ''
-
+// 로그인 시 저장되는 token 가져와서 사용하는 방식으로 수정 필요
+const REFRESH_TOKEN = ''
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
@@ -11,5 +12,20 @@ const axiosInstance = axios.create({
   },
   withCredentials: true,
 })
+
+export async function reissueToken() {
+  try {
+    const response = await axiosInstance.post('/auth/reissue', null, {
+      headers: {
+        RefreshToken: `Bearer ${REFRESH_TOKEN}`,
+      },
+    })
+    return response.data
+  } catch (error) {
+    const axiosError = error as AxiosError
+    // Handle errors here
+    throw axiosError
+  }
+}
 
 export default axiosInstance
