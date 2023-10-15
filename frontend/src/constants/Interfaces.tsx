df6b640dec8f324f2084f785b9530e4dac9aa7d8
mlnpaper <minpaper.dev@gmail.com>
2023-10-16 01:44:07 +0900

    Feat: interface 추가

--- frontend/src/constants/Interfaces.tsx
+++ frontend/src/constants/Interfaces.tsx
@@ -1,3 +1,12 @@
+export interface RecipeListItem {
+  id: number
+  title: string
+  mainImageUrl: string
+  content: string
+  heartCount: number
+  heart: boolean
+}
+
 export interface Recipe {
   content: string
   difficulty: string
@@ -25,6 +34,7 @@ export interface RecipeIngredientDtoList {
   [key: string]: string | number
 }
 
+// 내 정보 - 주소
 export interface Address {
   addressDetail: string
   lat: number
@@ -32,10 +42,104 @@ export interface Address {
   roadAddress: string
 }
 
+// 내 정보
 export interface Profile {
   address: Address
   email: string
   nickName: string
   phoneNumber: string
   profileUrl: string
+  isChef: boolean
+  point: number
+}
+
+// 요리사 정보
+export interface Chef {
+  chefId: number
+  distance: number
+  introduce: string
+  lat: number
+  lnt: number
+  nickname: string
+  profileUrl: string
+  recipeCount: number
+  reviewCount: number
+  starAvg: number
+  isQuotation?: number
+  quotationId?: number
+  requestId?: number
+}
+
+// 요청서
+export interface RequestForm {
+  content: string
+  requestFormId: number
+  title: string
+}
+
+// 주변 요청 정보
+export interface Requester {
+  distance: 0
+  expectedPrice: 0
+  lat: 0
+  lnt: 0
+  mainImageUrl: string
+  memberId: 0
+  nickname: string
+  profileUrl: string
+  requestId: 0
+  title: string
+}
+
+// 요청서 detail
+export interface RequestDetail {
+  address: string
+  addressDetail: string
+  content: string
+  expectedAt: string
+  expectedPrice: number
+  heartCount: number
+  ingredientList: string[]
+  mainImageUrl: string
+  recipeContent: string
+  recipeTitle: string
+  requestId: number
+  requesterNickname: string
+  title: string
+}
+
+// 견적서 목록
+export interface Quotation {
+  content: string
+  difficulty: string
+  expectedTime: number
+  quotationId: number
+  title: string
+}
+
+// 견적서 detail
+export interface QuotationDetail {
+  content: string
+  difficulty: string
+  expectedTime: number
+  quotationId: number
+  recipeDetailDtoList: string[]
+  recipeIngredientDtoList: RecipeIngredientDtoList[]
+  title: string
+}
+
+// select box
+export interface CustomSelectData {
+  value: string
+  text: string
+}
+
+export interface MarkerItem {
+  lat: number
+  lnt: number
+  nickname: string
+  starAvg: number
+  reviewCount: number
+  introduce: string
+  [key: string]: string | number
 }
