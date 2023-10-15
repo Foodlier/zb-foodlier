726b4ed7132cf18570837004633c4242d58b8e4c
mlnpaper <minpaper.dev@gmail.com>
2023-10-16 02:36:04 +0900

    Feat: 요리사가 보낸 견적서 조회 및 요청 취소 구현

--- frontend/src/components/refrigerator/ChefCard.tsx
+++ frontend/src/components/refrigerator/ChefCard.tsx
@@ -1,46 +1,99 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { useNavigate } from 'react-router-dom'
 import * as S from '../../styles/refrigerator/ChefCard.styled'
 import useIcon from '../../hooks/useIcon'
+import { Chef } from '../../constants/Interfaces'
+import { requireChefIdState } from '../../store/recoilState'
+import defaultProfile from '../../../public/images/default_profile.png'
+import axiosInstance from '../../utils/FetchCall'
 
-interface RequestedInterface {
-  chefId: number
-  profileUrl: string
-  nickName: string
-  introduce: string
-  starAvg: number
-  distance: number
-  reviewCount: number
-  recipeCount: number
+interface ChefCardProps {
+  chefItem: Chef
+  isRequest: boolean
+  setIsRequestModal: (value: boolean) => void
+  refresh: () => void
 }
 
-interface ChefType {
-  el: RequestedInterface
-  isRequest: string
-}
-
-const ChefCard: React.FC<ChefType> = ({ el, isRequest }) => {
+const ChefCard: React.FC<ChefCardProps> = ({
+  chefItem,
+  isRequest,
+  setIsRequestModal,
+  refresh,
+}) => {
+  const navigate = useNavigate()
   const { IcStar } = useIcon()
+
+  const [, setRequireChefId] = useRecoilState(requireChefIdState)
+
+  const cancelRequest = async () => {
+    const response = await axiosInstance.patch(
+      `/refrigerator/cancel/${chefItem.requestId}`
+    )
+    refresh()
+    console.log(response)
+  }
+
   return (
     <S.Card>
       <S.CardInfo className="card-info">
-        <img src="" alt="프로필 사진" className="mainImg" />
+        <img
+          src={chefItem.profileUrl || defaultProfile}
+          alt="프로필 사진"
+          className="mainImg"
+        />
         <S.ChefInfo className="chef-info">
           <S.ChefTopInfo className="top-info">
-            <span className="nickName">{el.nickName}</span>
+            <span className="nickName">{chefItem.nickname}</span>
             <IcStar size={1.8} />
-            <span className="rating">{`${el.starAvg}(${el.reviewCount})`}</span>
+            <span className="rating">{`${chefItem.starAvg}(${chefItem.reviewCount})`}</span>
           </S.ChefTopInfo>
           <S.ChefBottomInfo className="bottom-info">
-            {el.introduce}
+            {chefItem.introduce}
           </S.ChefBottomInfo>
         </S.ChefInfo>
       </S.CardInfo>
       <S.ElseInfo>
-        <span>{el.distance}m</span>
-        {isRequest === 'false' ? (
-          <S.RequestButton type="button">요청하기</S.RequestButton>
+        <span>{chefItem.distance}m</span>
+        {isRequest ? (
+          <S.FlexWrap>
+            {chefItem?.quotationId && (
+              <S.RequestedButton
+                type="button"
+                onClick={() =>
+                  navigate(
+                    `/refrigerator/quotation/detail/${chefItem.quotationId}`,
+                    {
+                      state: {
+                        requestId: chefItem?.requestId,
+                        chef: chefItem?.nickname,
+                      },
+                    }
+                  )
+                }
+                $isActive
+              >
+                견적서 보러가기
+              </S.RequestedButton>
+            )}
+            <S.RequestedButton
+              onClick={cancelRequest}
+              type="button"
+              $isActive={false}
+            >
+              요청 취소
+            </S.RequestedButton>
+          </S.FlexWrap>
         ) : (
-          <S.RequestedButton type="button">요청됨</S.RequestedButton>
+          <S.RequestButton
+            type="button"
+            onClick={() => {
+              setRequireChefId(chefItem.chefId)
+              setIsRequestModal(true)
+            }}
+          >
+            요청하기
+          </S.RequestButton>
         )}
       </S.ElseInfo>
     </S.Card>
