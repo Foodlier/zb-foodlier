2bbdede30f06f2ce361e211dec0f6e5b9162c137
J-HyeonSeo <jcs2349@gmail.com>
2023-10-15 20:07:40 +0900

    Feat: Profile및 포인트 거래 Validation추가

--- src/main/java/com/zerobase/foodlier/module/payment/dto/PaymentRequest.java
+++ src/main/java/com/zerobase/foodlier/module/payment/dto/PaymentRequest.java
@@ -7,6 +7,8 @@ import lombok.*;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.UUID;
 
@@ -16,9 +18,13 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class PaymentRequest {
+    @NotNull(message = "결제 수단을 입력해주세요.")
     @Enumerated(EnumType.STRING)
     private PayType payType;
+    @NotNull(message = "금액을 입력해주세요.")
+    @Min(value = 1000, message = "1000원 부터 충전이 가능합니다.")
     private Long amount;
+    @NotNull(message = "주문 유형을 입력해주세요.")
     @Enumerated(EnumType.STRING)
     private OrderNameType orderName;
 
