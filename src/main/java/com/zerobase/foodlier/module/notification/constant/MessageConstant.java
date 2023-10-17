ad1ee578e9ef8ff890c324e1c82d52911327e6ec
gomljo <gomljo0307@naver.com>
2023-10-17 23:20:03 +0900

    feat: 알림 메세지 생성 시 필요한 상수 정의
    알림 메세지 생성 시 필요한 상수 정의
    
    resolves: #256

---
+++ src/main/java/com/zerobase/foodlier/module/notification/constant/MessageConstant.java
@@ -0,0 +1,17 @@
+package com.zerobase.foodlier.module.notification.constant;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConstant {
+
+    public static final String DELIMITER = " ";
+    public static final String HONORIFIC_TITLE = "님이";
+    public static final String OPEN_BRACKET = "{";
+    public static final String CLOSE_BRACKET = "}";
+
+    private MessageConstant(){
+
+    }
+
+}
