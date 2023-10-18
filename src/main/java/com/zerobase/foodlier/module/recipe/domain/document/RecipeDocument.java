b8b1c6086f4e12f0e0ff460328e17cd68c2cded5
gomljo <gomljo0307@naver.com>
2023-10-18 18:28:31 +0900

    refactor: 검색 객체 수정 및 매핑 정보 수정
    검색 객체의 정보를 추가하기 위해 검색 객체 수정
    
    추가된 정보
    - 요리 소개글
    - 메인 이미지 url
    
    resolves: #279

--- src/main/java/com/zerobase/foodlier/module/recipe/domain/document/RecipeDocument.java
+++ src/main/java/com/zerobase/foodlier/module/recipe/domain/document/RecipeDocument.java
@@ -1,12 +1,10 @@
 package com.zerobase.foodlier.module.recipe.domain.document;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.elasticsearch.annotations.*;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import javax.persistence.Id;
 import java.time.LocalDateTime;
@@ -21,20 +19,26 @@ import java.time.LocalDateTime;
 @Setting(settingPath = "/elasticsearch/recipe-settings.json")
 public class RecipeDocument {
     private static final int MIN_COMMENT = 0;
+    private static final int MIN_HEART = 0;
     private static final String DATE_FORMAT = "uuuu-MM-dd'T'HH:mm:ss.SSS";
     @Id
     private Long id;
     private String title;
+    private String content;
     private String ingredients;
     private String writer;
     private long memberId;
     private long numberOfHeart;
     private long numberOfComment;
+    private String mainImageUrl;
     @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second, pattern = DATE_FORMAT)
     private LocalDateTime createAt;
 
     public void updateTitle(String title) {
         this.title = title;
+    }
+    public void updateContent(String content){
+        this.content = content;
     }
 
     public void updateIngredients(String ingredients) {
@@ -45,8 +49,16 @@ public class RecipeDocument {
         this.writer = writer;
     }
 
-    public void updateNumberOfHeart(long numberOfHeart) {
-        this.numberOfHeart = numberOfHeart;
+    public void updateMainImageUrl(String mainImageUrl) {
+        this.mainImageUrl = mainImageUrl;
+    }
+
+    public void plusNumberOfHeart() {
+        this.numberOfHeart++;
+    }
+
+    public void minusNumberOfHeart() {
+        this.numberOfHeart = Math.max(MIN_HEART, --this.numberOfHeart);
     }
 
     public void plusNumberOfComment() {
