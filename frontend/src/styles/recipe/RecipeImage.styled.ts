480d0a92c2e345bd5ea3b9edbfa4445a7c8e4d88
mlnpaper <minpaper.dev@gmail.com>
2023-10-18 14:59:56 +0900

    Fix: 레시피 사진 등록 가능하도록 수정

--- frontend/src/styles/recipe/RecipeImage.styled.ts
+++ frontend/src/styles/recipe/RecipeImage.styled.ts
@@ -1,7 +1,30 @@
 import styled from 'styled-components'
 import { palette } from '../../constants/Styles'
 
-export const ImageButton = styled.button<{ $size: number }>`
+export const Label = styled.label<{ $size: number }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: ${props => props.$size || 0}rem;
+  height: ${props => props.$size || 0}rem;
+  border-radius: 1rem;
+  background-color: #f3f3f3;
+  border: 1px solid ${palette.divider};
+  cursor: pointer;
+`
+
+export const ImageButton = styled.input<{ $size: number }>`
+  display: none;
+`
+
+export const SubText = styled.span`
+  font-size: 1.2rem;
+  color: ${palette.textSecondary};
+  margin-top: 2rem;
+`
+
+export const EmptyImage = styled.button<{ $size: number }>`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -13,8 +36,9 @@ export const ImageButton = styled.button<{ $size: number }>`
   border: 1px solid ${palette.divider};
 `
 
-export const SubText = styled.span`
-  font-size: 1.2rem;
-  color: ${palette.textSecondary};
-  margin-top: 2rem;
+export const Image = styled.img`
+  width: 20rem;
+  height: 20rem;
+  border: 1px solid ${palette.divider};
+  border-radius: 1rem;
 `
