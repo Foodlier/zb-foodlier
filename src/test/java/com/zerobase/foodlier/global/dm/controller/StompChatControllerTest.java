71daececccad763c8eb3d228f0e5c88e7f03b124
Taewon Hwang <htw1203@gmail.com>
2023-10-19 20:27:11 +0900

    Test: StompChatController 테스트코드 작성

--- src/test/java/com/zerobase/foodlier/global/dm/controller/StompChatControllerTest.java
+++ src/test/java/com/zerobase/foodlier/global/dm/controller/StompChatControllerTest.java
@@ -3,35 +3,28 @@ package com.zerobase.foodlier.global.dm.controller;
 import com.zerobase.foodlier.module.dm.dm.dto.MessagePubDto;
 import com.zerobase.foodlier.module.dm.dm.dto.MessageSubDto;
 import com.zerobase.foodlier.module.dm.dm.service.DmService;
+import com.zerobase.foodlier.module.dm.dm.type.MessageType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessageType;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public class StompChatControllerTest {
-
-    static final String CHATTING_URI = "http://localhost:8080/ws";
-    static final String CHATTING_SUB = "/sub/message";
+@ExtendWith(MockitoExtension.class)
+class StompChatControllerTest {
 
     @Mock
     private DmService dmService;
@@ -42,33 +35,61 @@ public class StompChatControllerTest {
     @InjectMocks
     private StompChatController stompChatController;
 
-    private MockMvc mockMvc;
+    @Test
+    @DisplayName("메세지 전송 성공")
+    void success_create_message() throws InterruptedException, ExecutionException {
+        // given
+        MessagePubDto messagePubDto = MessagePubDto.builder()
+                .roomId(1L)
+                .message("hi")
+                .writer("user")
+                .messageType(MessageType.CHAT)
+                .build();
 
-//    public StompChatControllerTest() {
-//        MockitoAnnotations.initMocks(this);
-//        mockMvc = MockMvcBuilders.standaloneSetup(stompChatController).build();
-//    }
+        MessageSubDto messageSubDto = MessageSubDto.builder()
+                .roomId(1L)
+                .dmId(2L)
+                .message("hi")
+                .writer("user")
+                .messageType(MessageType.CHAT)
+                .createdAt(LocalDateTime.of(2023, 10, 19, 19, 25, 30))
+                .build();
 
-//    @Test
-//    public void testMessage() throws Exception {
-//        // Arrange
-//        MessagePubDto message = new MessagePubDto(/* Initialize with required data */);
-//        MessageSubDto messageSubDto = new MessageSubDto(/* Initialize with expected data */);
-//
-//        given(dmService.createDm(any(MessagePubDto.class)))
-//                .willReturn(CompletableFuture.completedFuture(messageSubDto));
-//
-//        // Create a simulated WebSocket message
-//        Map<String, Object> headers = new HashMap<>();
-//        headers.put(SimpMessageHeaderAccessor.MESSAGE_TYPE_HEADER, SimpMessageType.MESSAGE);
-//        MessageHeaders messageHeaders = new MessageHeaders(headers);
-//
-//        // Act
-//        stompChatController.message(message);
-//
-//        // Assert
-//        verify(dmService, times(1)).createDm(any(MessagePubDto.class));
-//        verify(template, times(1)).convertAndSend(anyString(), any(MessageSubDto.class));
-//    }
+        CompletableFuture<MessageSubDto> future = CompletableFuture
+                .completedFuture(messageSubDto);
+
+        given(dmService.createDm(messagePubDto)).willReturn(future);
+
+        // when
+        stompChatController.createMessage(messagePubDto);
+
+        // then
+        ArgumentCaptor<String> destinationCaptor =
+                ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<MessageSubDto> messageSubDtoCaptor =
+                ArgumentCaptor.forClass(MessageSubDto.class);
+
+        verify(dmService, times(1)).createDm(messagePubDto);
+        verify(template, times(1))
+                .convertAndSend(destinationCaptor.capture(), messageSubDtoCaptor.capture());
+
+        MessageSubDto messageSubDtoCaptorValue = messageSubDtoCaptor.getValue();
+
+        assertAll(
+                () -> assertEquals("/sub/message/" + messagePubDto.getRoomId(),
+                        destinationCaptor.getValue()),
+                () -> assertEquals(messagePubDto.getRoomId(),
+                        messageSubDtoCaptorValue.getRoomId()),
+                () -> assertEquals(messagePubDto.getMessage(),
+                        messageSubDtoCaptorValue.getMessage()),
+                () -> assertEquals(messagePubDto.getWriter(),
+                        messageSubDtoCaptorValue.getWriter()),
+                () -> assertEquals(messagePubDto.getMessageType(),
+                        messageSubDtoCaptorValue.getMessageType()),
+                () -> assertEquals(2L,
+                        messageSubDtoCaptorValue.getDmId()),
+                () -> assertEquals("2023-10-19T19:25:30",
+                        messageSubDtoCaptorValue.getCreatedAt().toString())
+        );
+    }
 }
-
