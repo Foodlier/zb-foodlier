9ce227d9e70835bb565b3f5c269de4211541f5d4
mlnpaper <minpaper.dev@gmail.com>
2023-10-21 04:15:12 +0900

    Fix: 헤더 UI 수정 및 알림 side

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,16 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import logo from '../../public/images/foodlier_logo.png'
 import useIcon from '../hooks/useIcon'
 import { palette } from '../constants/Styles'
-import NotificationItem from './notification/NotificationItem'
 import * as S from '../styles/Header.styled'
 import MoSearch from './search/MoSearch'
+import SideNotification from './SideNotification'
 
 const Header = () => {
   const navigate = useNavigate()
-  const { IcSearch, IcBell } = useIcon()
+  const { IcSearch } = useIcon()
 
-  const notiRef = useRef<HTMLDivElement | null>(null)
   const [isToggle, setIsToggle] = useState(false)
 
   useEffect(() => {
@@ -41,6 +40,7 @@ const Header = () => {
     },
     { title: '냉마카세', navigate: 'refrigerator' },
     { title: '채팅', navigate: 'chat' },
+    { title: '마이페이지', navigate: 'my' },
   ]
 
   const navigateTo = (pageName: string) => {
@@ -52,38 +52,23 @@ const Header = () => {
       <S.LogoButton onClick={() => navigateTo('')}>
         <S.Logo src={logo} alt="로고 이미지" />
       </S.LogoButton>
+      {/* mobile */}
       <S.WrapIcon>
-        <S.WrapNotification>
-          <S.Icon onClick={() => setIsToggle(!isToggle)}>
-            <IcBell size={3} color={palette.textPrimary} />
-          </S.Icon>
-          <S.Notification $isToggle={isToggle}>
-            <NotificationItem />
-          </S.Notification>
-        </S.WrapNotification>
-        {/* 모바일 검색 - onClick 추가 */}
         <S.Icon onClick={toggleMoSearch}>
           <IcSearch size={3} color={palette.textPrimary} />
         </S.Icon>
       </S.WrapIcon>
+
+      {/* web */}
       <S.WrapMenu>
         {HEADER_MENU_LIST.map(item => (
           <S.Menu key={item.title} onClick={() => navigateTo(item.navigate)}>
             {item.title}
           </S.Menu>
         ))}
-        <S.WrapNotification ref={notiRef}>
-          <S.Button onClick={() => setIsToggle(!isToggle)}>
-            <IcBell size={3} color={palette.textPrimary} />
-          </S.Button>
-          <S.Notification $isToggle={isToggle}>
-            <NotificationItem />
-          </S.Notification>
-        </S.WrapNotification>
-        <S.LoginButton onClick={() => navigateTo('login')}>LOGIN</S.LoginButton>
       </S.WrapMenu>
-      {/* 모바일 검색 - MoSearch 컴포넌트 추가  */}
       {isMoSearchOpen && <MoSearch setIsMoSearchOpen={setIsMoSearchOpen} />}
+      <SideNotification />
     </S.Container>
   )
 }
