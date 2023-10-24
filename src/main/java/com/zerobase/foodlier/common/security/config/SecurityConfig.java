0b5c3efb9da9ef209fbd31370d396142d934c459
SeungHyunLee054 <yellow13lsh@naver.com>
2023-10-24 20:15:25 +0900

    Fix: webMvcConfig 삭제
    #288

--- src/main/java/com/zerobase/foodlier/common/security/config/SecurityConfig.java
+++ src/main/java/com/zerobase/foodlier/common/security/config/SecurityConfig.java
@@ -101,6 +101,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
+                "http://127.0.0.1:8080",
+                "http://localhost:8080",
                 "https://zb-foodlier.vercel.app",
                 "http://ec2-15-165-55-217.ap-northeast-2.compute.amazonaws.com",
                 "http://ec2-13-209-238-113.ap-northeast-2.compute.amazonaws.com"
