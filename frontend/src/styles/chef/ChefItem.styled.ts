ddd603875e27f0012c4daa3bfa43ed97c28623a9
mlnpaper <minpaper.dev@gmail.com>
2023-10-24 15:14:15 +0900

    Feat: 메인 Top Chef API 조회 연동

--- frontend/src/styles/chef/ChefItem.styled.ts
+++ frontend/src/styles/chef/ChefItem.styled.ts
@@ -6,6 +6,8 @@ export const Container = styled.section`
   flex-direction: column;
   align-items: center;
   margin-right: 3rem;
+
+  cursor: pointer;
 `
 export const Image = styled.img`
   width: 10rem;
@@ -13,7 +15,6 @@ export const Image = styled.img`
   max-width: 100%;
   max-height: 10rem;
   border-radius: 50%;
-  border: 0.3rem solid ${palette.main};
   padding: 15%;
 `
 export const Nickname = styled.p`
