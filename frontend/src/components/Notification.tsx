e362b551ce1778d43a94ccaa6f955e494186aa89
mlnpaper <minpaper.dev@gmail.com>
2023-10-24 15:19:30 +0900

    Fix: 알림 요소 스타일 수정

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { palette, zindex } from '../constants/Styles'
 
 // 스타일드 컴포넌트를 사용하여 알림 스타일을 정의
 const NotificationWrapper = styled.div<{ $show: boolean; $index: number }>`
+  width: 30rem;
   position: fixed;
-  top: ${props => props.$index * 10}rem;
+  top: 10rem;
   right: 2rem;
   background-color: ${palette.white};
   color: ${palette.textPrimary};
@@ -16,33 +16,20 @@ const NotificationWrapper = styled.div<{ $show: boolean; $index: number }>`
   z-index: ${zindex.modal}; /* 알림이 화면 위에 나타나도록 함 */
   transition: all 0.3s ease-in-out;
   margin-bottom: 1rem;
+  box-shadow: 1px 1px 4px ${palette.shadow};
 
   /* 알림이 나타날 때의 애니메이션 */
-  display: ${props => (props.$show ? 'block' : 'none')};
+  opacity: ${props => (props.$show ? 1 : 0)};
 `
 
 interface NotificationProps {
   text: string
-  index: number
+  show: boolean
+  setShow: (value: boolean) => void
 }
 
-const Notification: React.FC<NotificationProps> = ({ text, index }) => {
-  const [show, setShow] = useState(true)
-
-  useEffect(() => {
-    // 5초 후에 알림을 숨김
-    const timer = setTimeout(() => {
-      setShow(false)
-    }, 5000)
-
-    return () => clearTimeout(timer)
-  }, [])
-
-  return (
-    <NotificationWrapper $show={show} $index={index}>
-      {text}
-    </NotificationWrapper>
-  )
+const Notification: React.FC<NotificationProps> = ({ text, show, setShow }) => {
+  return <NotificationWrapper $show={show}>{text}</NotificationWrapper>
 }
 
 export default Notification
