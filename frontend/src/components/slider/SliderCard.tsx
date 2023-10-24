f1a23589a1856c8b9b0290f2f410923bf9df1bfc
mlnpaper <minpaper.dev@gmail.com>
2023-10-24 23:52:53 +0900

    Fix: 메인 슬라이더 좋아요 가능하도록 수정

--- frontend/src/components/slider/SliderCard.tsx
+++ frontend/src/components/slider/SliderCard.tsx
@@ -1,11 +1,39 @@
+import { useState } from 'react'
 import * as S from '../../styles/slider/SliderCard.styled'
 import '../../reset.css'
 import useIcon from '../../hooks/useIcon'
 import { palette } from '../../constants/Styles'
 import { RecipeListItem } from '../../constants/Interfaces'
+import axiosInstance from '../../utils/FetchCall'
 
 function SliderCard({ item }: { item: RecipeListItem }) {
   const { IcFavorite, IcFavoriteFill } = useIcon()
+
+  const [isLike, setIsLike] = useState(item.isHeart)
+  const [likeCount, setLikeCount] = useState(item.heartCount)
+
+  const postLike = async () => {
+    const res = await axiosInstance.post(`/api/recipe/heart/${item.recipeId}`)
+    console.log(res)
+  }
+
+  const deleteLike = async () => {
+    const res = await axiosInstance.delete(`/api/recipe/heart/${item.recipeId}`)
+    console.log(res)
+  }
+
+  const onClickLikeButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    if (isLike) {
+      deleteLike()
+      setIsLike(false)
+      setLikeCount(likeCount - 1)
+    } else {
+      postLike()
+      setIsLike(true)
+      setLikeCount(likeCount + 1)
+    }
+  }
 
   return (
     <S.BoxContainer>
@@ -13,13 +41,13 @@ function SliderCard({ item }: { item: RecipeListItem }) {
       <S.TextWrap>
         <S.FlexWrap>
           <S.Title>{item.title}</S.Title>
-          <S.Like>
-            {item.isHeart ? (
+          <S.Like onClick={onClickLikeButton}>
+            {isLike ? (
               <IcFavoriteFill size={2.5} color={palette.main} />
             ) : (
               <IcFavorite size={2.5} color={palette.textPrimary} />
             )}
-            {item.heartCount}
+            {likeCount}
           </S.Like>
         </S.FlexWrap>
         <S.Content>{item.content}</S.Content>
