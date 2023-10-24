bbb63d9648f865fc4e8c536c47aa5ad1b8b5b76c
SeungHyunLee054 <yellow13lsh@naver.com>
2023-10-24 18:29:20 +0900

    Fix: @EnableWebMvc 삭제
    #288

--- src/main/java/com/zerobase/foodlier/common/security/config/WebMvcConfig.java
+++ src/main/java/com/zerobase/foodlier/common/security/config/WebMvcConfig.java
@@ -2,17 +2,13 @@ package com.zerobase.foodlier.common.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        WebMvcConfigurer.super.addCorsMappings(registry);
-
         registry.addMapping("/**")
                 .allowedHeaders("*")
                 .allowedOrigins("https://zb-foodlier.vercel.app")
@@ -20,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:8080")
                 .allowedOrigins("http://ec2-15-165-55-217.ap-northeast-2.compute.amazonaws.com")
                 .allowedOrigins("http://ec2-13-209-238-113.ap-northeast-2.compute.amazonaws.com")
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "HEAD", "OPTIONS")
+                .allowedMethods("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true);
     }
