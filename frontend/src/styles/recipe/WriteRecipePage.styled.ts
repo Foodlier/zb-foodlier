96be4028ba0adc7b8fbc93e0e00ab3ef71111967
mlnpaper <minpaper.dev@gmail.com>
2023-11-16 19:08:35 +0900

    Fix: 사진 수정 가능하도록

--- frontend/src/styles/recipe/WriteRecipePage.styled.ts
+++ frontend/src/styles/recipe/WriteRecipePage.styled.ts
@@ -225,3 +225,57 @@ export const ErrorText = styled.span`
     font-size: ${typography.web.desc};
   }
 `
+
+export const EditImage = styled.button`
+  margin-top: 1rem;
+`
+
+export const EditDetailImage = styled.button`
+  margin-top: 0.2rem;
+  font-size: 1.2rem;
+`
+
+export const Image = styled.img<{ $size: number }>`
+  width: ${props => props.$size || 0}rem;
+  height: ${props => props.$size || 0}rem;
+  min-width: ${props => props.$size || 0}rem;
+  min-height: ${props => props.$size || 0}rem;
+  object-fit: cover;
+  border: 1px solid ${palette.divider};
+  border-radius: 1rem;
+`
+
+export const Label = styled.label<{ $size: number }>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: ${props => props.$size || 0}rem;
+  height: ${props => props.$size || 0}rem;
+  min-width: ${props => props.$size || 0}rem;
+  min-height: ${props => props.$size || 0}rem;
+
+  border-radius: 1rem;
+  background-color: #f3f3f3;
+  border: 1px solid ${palette.divider};
+  cursor: pointer;
+`
+
+export const ImageButton = styled.input`
+  display: none;
+`
+
+export const SubText = styled.span`
+  font-size: ${typography.mobile.desc};
+  color: ${palette.textSecondary};
+  margin-top: 2rem;
+
+  ${breakpoints.large} {
+    font-size: ${typography.web.desc};
+  }
+`
+
+export const WrapEdit = styled.div`
+  display: flex;
+  flex-direction: column;
+`
