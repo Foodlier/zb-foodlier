4825451bf1a6cdb9ff90bb59123b6682b66e89be
d-bCODING <rlaehq@gmail.com>
2023-12-15 15:35:32 +0900

    Fix : sockJS 연결 주소 변경

--- frontend/src/components/chat/ChatRoom.tsx
+++ frontend/src/components/chat/ChatRoom.tsx
@@ -131,9 +131,7 @@ const ChatRoom = ({ roomNum }: { roomNum: number | undefined }) => {
   // 방 정보 새로 업데이트 될 때 마다(방 클릭시 마다)
   useEffect(() => {
     // 채팅 환경설정
-    const socket = new SockJS(
-      'https://foodlier.store/ws'
-    )
+    const socket = new SockJS('https://foodlier.store/ws')
     const stompClient = StompJs.over(socket)
     setStompClientstate(stompClient)
     // 메세지 수신
